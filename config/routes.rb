abf9355ffce77239926d4cc2df7faf6dd733dfb8
Filipe Lins <frnlins@gmail.com>
2014-07-07 13:41:36 -0300

    correção de 3 testes correspondentes a funcionalidade de soma

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get 'calculadora_machine/index'
+  get 'calculadora_machine/new'
+  post 'calculadora_machine/create'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
